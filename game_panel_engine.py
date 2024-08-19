29420d285b715cf171053ddbcbd0cc4428ab8c22
Гершман Антон Лейбович <agershman@corp.local>
2024-08-19 17:51:36 +0300

    Работает позиционирование канвы

--- game_panel_engine.py
+++ game_panel_engine.py
@@ -1,13 +1,32 @@
 import tkinter as Tk
 
-class GameBoard(Tk.Frame):
-    def __init__(self):
-        super().__init__()
-        self.canvas = Game()
+class GameCanvas(Tk.Canvas):
+    def __init__(self, frame_width: int, frame_height: int):
+        super().__init__(
+            width=frame_width,
+            height=frame_height
+        )
+        self.initGame()
         self.pack()
 
-class Game(Tk.Canvas):
-    _left: int
-    _top: int
-    _width: int
-    _height: int
+    def initGame(self):
+        pass
+
+class GameWidget(Tk.Frame):
+    def __init__(self, frame_width: int, frame_height: int):
+        super().__init__()
+        self.centerWindow(frame_width, frame_height)
+        self.canvas = self.createCanvas(frame_width, frame_height)
+        self.pack(expand=True)
+
+    def centerWindow(self, frame_width:int, frame_height:int):
+        screen_width = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
+
+        x = (screen_width - frame_width) // 2
+        y = (screen_height - frame_height) // 2
+
+        self.master.geometry(f'{frame_width}x{frame_height}+{x}+{y}')
+
+    def createCanvas(self, frame_width, frame_height) -> GameCanvas:
+        return GameCanvas(frame_width, frame_height)
--- snake.py
+++ snake.py
@@ -1,9 +1,20 @@
 import tkinter as tk
 import game_panel_engine as game
 
+class SnakeCanvas(game.GameCanvas):
+    def initGame(self):
+        self.create_oval(0, 0, 800, 600)
+
+class SnakeWidget(game.GameWidget):
+    def __init__(self):
+        super().__init__(800, 600)
+
+    def createCanvas(self, frame_width, frame_height):
+        return SnakeCanvas(frame_width, frame_height)
+
 def main():
     root = tk.Tk()
-    snake = game.GameBoard()
+    snake = SnakeWidget()
     snake.master.title('Змейка')
     root.mainloop()
 
