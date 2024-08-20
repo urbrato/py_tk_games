19f3f5cffdf5e190208bfaeee0b5494a17f210f5
Гершман Антон Лейбович <allegecityrat@yandex.ru>
2024-08-20 22:20:35 +0300

    Документирование

--- snake.py
+++ snake.py
@@ -2,33 +2,69 @@ import tkinter as tk
 import game_panel_engine as game
 
 class GameObject:
+    '''
+    Игровой объект
+    '''
     def __init__(self, x: int, y: int):
+        '''
+        Конструктор игрового объекта
+
+        :param x: номер клетки объекта по горизонтали
+        :type x: int
+        :param y: номер клетки объекта по вертикали
+        :type y: int
+        '''
         self.x = x
         self.y = y
 
 class SnakeCanvas(game.GameCanvas):
+    '''
+    Игровая канва (основной класс игры)
+    '''
     def init_game(self):
+        '''
+        Первоначальная настройка игры
+        '''
         self.master.title('Змейка')
         self.set_board_size_by_cells_size(20)
         self.create_game()
 
     def create_game(self):
+        '''
+        Настройка параметров игры
+        '''
         self.create_board()
 
     def create_board(self):
+        '''
+        Настройка игрового поля
+        '''
         self.set_all_cells_color('beige')
 
 class SnakeWidget(game.GameWidget):
+    '''
+    Виджет игры
+    '''
     def __init__(self):
+        '''
+        Конструктор виджета
+        '''
         super().__init__(800, 600)
 
     def create_canvas(self):
+        '''
+        Создание нужной канвы
+        '''
         return SnakeCanvas(self)
 
 def main():
+    '''
+    Окно игры
+    '''
     root = tk.Tk()
     snake = SnakeWidget()
     root.mainloop()
 
+# Запускатор игры
 if __name__ == '__main__':
     main()
