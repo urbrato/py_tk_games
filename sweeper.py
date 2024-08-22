cfa2e98d7d9db550cfffc7e8efe004d2631cab2d
Гершман Антон Лейбович <agershman@corp.local>
2024-08-22 18:12:39 +0300

    Игровой объект

--- sweeper.py
+++ sweeper.py
@@ -1,19 +1,53 @@
 import tkinter as tk
 import game_panel_engine as game
 
+class GameObject:
+    '''
+    Игровой объект
+    '''
+    def __init__(self, x: int, y: int):
+        '''
+        Конструктор игрового объекта
+
+        :param x: номер клетки объекта по горизонтали
+        :type x: int
+        :param y: номер клетки объекта по вертикали
+        :type y: int
+        '''
+        self.x = x
+        self.y = y
+
 class SweeperCanvas(game.GameCanvas):
+    '''
+    Игровая канва
+    '''
     def init_game(self):
+        '''
+        Инициализация игры
+        '''
         self.master.title('Сапёр')
         self.set_board_size_by_cells_size(25)
 
 class SweeperWidget(game.GameWidget):
+    '''
+    Игровой виджет
+    '''
     def __init__(self):
+        '''
+        Конструктор
+        '''
         super().__init__(800, 600)
 
     def create_canvas(self):
+        '''
+        Создание канвы
+        '''
         return SweeperCanvas(self)
     
 def main():
+    '''
+    Запускатор
+    '''
     root = tk.Tk()
     game = SweeperWidget()
     root.mainloop()
