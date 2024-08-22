be8c026fe623a8d868ebab7b9b3a0c78376c1ec6
Гершман Антон Лейбович <agershman@corp.local>
2024-08-22 18:43:56 +0300

    Игровой объект рисует себя. Создан список игровых объектов.

--- sweeper.py
+++ sweeper.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 import game_panel_engine as game
 
+class SweeperCanvas(game.GameCanvas): pass
+
 class GameObject:
     '''
     Игровой объект
     '''
-    def __init__(self, x: int, y: int):
+
+    _INTACT_IMAGE = None
+
+    def __init__(self, x: int, y: int, canvas: SweeperCanvas):
         '''
         Конструктор игрового объекта
 
@@ -13,9 +18,21 @@ class GameObject:
         :type x: int
         :param y: номер клетки объекта по вертикали
         :type y: int
+        :param canvas: игровая канва
+        :type canvas: SweeperCanvas
         '''
         self.x = x
         self.y = y
+        self._canvas = canvas
+
+    def draw(self):
+        '''
+        Отрисовка объекта
+        '''
+        if self._INTACT_IMAGE == None:
+            self._INTACT_IMAGE = tk.PhotoImage(file='sweeper_img\\intact.png')
+        image = self._INTACT_IMAGE
+        self._canvas.draw_image(self.x, self.y, image)
 
 class SweeperCanvas(game.GameCanvas):
     '''
@@ -27,6 +44,18 @@ class SweeperCanvas(game.GameCanvas):
         '''
         self.master.title('Сапёр')
         self.set_board_size_by_cells_size(25)
+        self.set_all_cells_border_color('silver')
+        self.create_game()
+
+    def create_game(self):
+        self.game_field: list[list[GameObject]]
+        self.game_field = []
+        for x in range(self._width):
+            self.game_field.append([])
+            for y in range(self._height):
+                gmo = GameObject(x, y, self)
+                self.game_field[x].append(gmo)
+                gmo.draw()
 
 class SweeperWidget(game.GameWidget):
     '''
