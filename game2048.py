10ed52c2caf8b7163fcf7b73f6b06e8111ef8b44
Гершман Антон Лейбович <allegecityrat@yandex.ru>
2024-08-23 17:03:23 +0300

    Начали делать игру 2048

---
+++ game2048.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import game_panel_engine as game
+
+class Game2048Canvas(game.GameCanvas):
+    '''
+    Игровая канва
+    '''
+    def init_game(self):
+        '''
+        Инициализация игры
+        '''
+        self.master.title('2048')
+        self.set_board_size_by_cells_count(4, 4)
+
+class Game2048Widget(game.GameWidget):
+    '''
+    Игровой виджет
+    '''
+    def __init__(self):
+        '''
+        Конструктор
+        '''
+        super().__init__(500, 500)
+
+    def create_canvas(self):
+        '''
+        Создание игровой канвы
+        '''
+        return Game2048Canvas(self)
+    
+def main():
+    '''
+    Запускатор
+    '''
+    root = tk.Tk()
+    game = Game2048Widget()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
